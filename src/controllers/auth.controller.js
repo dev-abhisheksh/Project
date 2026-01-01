014468d23fd2c74ed6384e58e60bda8cc5c21f4d
Abhishek Sharma <abhisheksharmaazpm2@gmail.com>
2026-01-02 00:14:14 +0530

    feat(auth): add refreshToken rotation controller and route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import hashToken from "../utils/hashToken.js";
 
 export const generateAccessToken = (user) => {
     return jwt.sign(
@@ -64,20 +65,110 @@ const registerUser = async (req, res) => {
 const loginWithPassword = async (req, res) => {
     try {
         let { email, password } = req.body;
-        if (!email || !password) return res.status(400).json({ message: "All fields are required" })
 
-        email = email.trim().toLowerCase()
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
 
-        const existingUser = await User.findOne({ email }).select("+password")
-        if (!existingUser) return res.status(404).json({ message: "User not found. Please Sign up" })
+        email = email.trim().toLowerCase();
 
-        const isPasswordValid = await bcrypt.compare(password, existingUser?.password)
-        if (!isPasswordValid) return res.status(400).json({ message: "Incorrect Email or Password" })
+        const existingUser = await User
+            .findOne({ email })
+            .select("+password");
 
-        const accessToken = generateAccessToken(existingUser)
-        const refreshToken = generateRefreshToken(existingUser)
+        if (!existingUser) {
+            return res
+                .status(401)
+                .json({ message: "Invalid email or password" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(
+            password,
+            existingUser.password
+        );
+
+        if (!isPasswordValid) {
+            return res
+                .status(401)
+                .json({ message: "Invalid email or password" });
+        }
+
+        const accessToken = generateAccessToken(existingUser);
+        const refreshToken = generateRefreshToken(existingUser);
+
+        existingUser.refreshTokens.push({
+            tokenHash: hashToken(refreshToken),
+            expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        });
+
+        await existingUser.save();
 
         res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+
+        return res.status(200).json({
+            message: "Login successful",
+            accessToken,
+        });
+    } catch (error) {
+        console.error("Failed to login user:", error);
+        return res.status(500).json({ message: "Failed to login user" });
+    }
+};
+
+const refreshAccessToken = async (req, res) => {
+    try {
+        const refreshToken = req.cookies?.refreshToken;
+        if (!refreshToken) {
+            return res.status(401).json({
+                message: "No refreshToken provided"
+            })
+        }
+
+        const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
+        if (!decoded?.userId) {
+            return res.status(403).json({
+                message: "Invlaid refresh token"
+            })
+        }
+
+        const refreshTokenHash = hashToken(refreshToken)
+
+        const user = await User.findOne({
+            _id: decoded.userId,
+            "refreshTokens.tokenHash": refreshTokenHash
+        })
+
+        if (!user) {
+            await User.updateOne(
+                { _id: decoded.userId },
+                { $set: { refreshTokens: [] } }
+            )
+
+            return res.status(403).json({
+                message: "Refresh Token reuse detected. Please login again"
+            })
+        }
+
+        user.refreshTokens = user.refreshTokens.filter(
+            (t) => t.tokenHash !== refreshTokenHash
+        )
+
+        const newAccessToken = generateAccessToken(user)
+        const newRefreshToken = generateAccessToken(user)
+
+        user.refreshTokens.push({
+            tokenHash: hashToken(newRefreshToken),
+            expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+        })
+
+        await user.save()
+
+        res.cookie("refreshToken", newRefreshToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             sameSite: "strict",
@@ -85,16 +176,18 @@ const loginWithPassword = async (req, res) => {
         })
 
         return res.status(200).json({
-            message: "Login successful",
-            accessToken,
+            accessToken: newAccessToken,
         });
     } catch (error) {
-        console.error("Failed to login user", error)
-        return res.status(500).json({ message: "Failed to login user" })
+        return res.status(403).json({
+            message: "Session expired. Please login again."
+        });
     }
 }
 
+
 export {
     registerUser,
-    loginWithPassword
+    loginWithPassword,
+    refreshAccessToken
 }
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { loginWithPassword, registerUser } from "../controllers/auth.controller.js";
+import { loginWithPassword, refreshAccessToken, registerUser } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginWithPassword)
+router.post("/refresh", refreshAccessToken)
 
 export default router;

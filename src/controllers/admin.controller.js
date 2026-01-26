8d392d96a530138a8a996e44cc31e15a57fc3104
Abhishek Sharma <abhisheksharmaazpm2@gmail.com>
2026-01-26 11:16:49 +0530

    feat(admin): add approveExpertApplication endpoint for processing expert applications

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,4 +1,5 @@
 import { ExpertApplication } from "../models/expertApplication.model.js"
+import { User } from "../models/user.model.js"
 
 const expertApplicationRequests = async (req, res) => {
     try {
@@ -22,6 +23,45 @@ const expertApplicationRequests = async (req, res) => {
     }
 }
 
-export{
-    expertApplicationRequests
+const approveExpertApplication = async (req, res) => {
+    try {
+        if (req.user.role !== "admin") {
+            return res.status(403).json({ message: "Only Admins are allowed" })
+        }
+
+        const { applicationId } = req.params;
+        const application = await ExpertApplication.findById(applicationId);
+        if (!application || application.status !== "pending") {
+            return res.status(404).json({ message: "Application not found or already processed" })
+        }
+
+        await User.findByIdAndUpdate(
+            application.userId,
+            {
+                $set: {
+                    role: "expert",
+                    isExpertVerified: true,
+                    bio: application.bio,
+                    experience: application.experience,
+                    expertCategories: application.expertCategories,
+                    portfolioLink: application.portfolioLink
+                }
+            }
+        )
+
+        application.status = "approved",
+            application.reviewedBy = req.user._id,
+            await application.save()
+
+        return res.status(200).json({ message: "Expert application approved successfully" })
+
+    } catch (error) {
+        console.error("Failed to approve expert application", error)
+        return res.status(500).json({ message: "Failed to approve expert application" })
+    }
+}
+
+export {
+    expertApplicationRequests,
+    approveExpertApplication
 }
--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import verifyToken from "../middlewares/auth.middleware.js";
 import authorizeRoles from "../middlewares/roles.middleware.js";
-import { expertApplicationRequests } from "../controllers/admin.controller.js";
+import { approveExpertApplication, expertApplicationRequests } from "../controllers/admin.controller.js";
 
 const router = express.Router();
 
 router.get("/expert-applications", verifyToken, authorizeRoles("admin"), expertApplicationRequests)
+router.patch("/expert-applications/:applicationId/approve", verifyToken, authorizeRoles("admin"), approveExpertApplication)
 
 export default router;

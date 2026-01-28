3cb64d8bd9a01d8842c8a9f84db41df1d28fe3c1
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-01-28 21:16:25 +0530

    feat(server): attach Socket.IO by creating HTTP server wrapper

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
+import { Server } from "socket.io";
 import app from "./src/app.js";
 import connectDB from "./src/utils/db.js";
 import dotenv from 'dotenv'
+import http from "http"
+
 
 dotenv.config();
 
 connectDB()
 
-app.listen(process.env.PORT, () => {
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log("New Client connected:", socket.id)
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected:", socket.id)
+    })
+})
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
     console.log(`Server running on port: ${process.env.PORT}`)
 })

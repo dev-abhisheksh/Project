c69f3bc07e37ef9499402a49850c99064ea5955c
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-02-07 10:59:19 +0530

    refactor(ban): remove debug logs from checkBan middleware

--- src/middlewares/checkBan.middleware.js
+++ src/middlewares/checkBan.middleware.js
@@ -1,17 +1,12 @@
 import { isUserBanned } from "../utils/isUserBanned.js";
 
 export const checkBan = (req, res, next) => {
-    console.log("checkBan running");
-    console.log("User:", req.user);
 
     if (isUserBanned(req.user)) {
-        console.log("User is banned");
         return res.status(403).json({
             message: "User banned temporary",
             bannedUntil: req.user.banExpiresAt
         });
     }
-
-    console.log("User allowed");
     next();
 }
